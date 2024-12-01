cd759025801aabdb10f2779df891d8017cd6499e
thevpc <taha.bensalah@gmail.com>
2024-12-01 20:52:34 +0100

    [INIT] preparing initial repo

--- app/app-jar/src/main/java/net/thevpc/samples/springnuts/SpringNutsApplication.java
+++ app/app-jar/src/main/java/net/thevpc/samples/springnuts/SpringNutsApplication.java
@@ -1,7 +1,9 @@
 package net.thevpc.samples.springnuts;
 
 import net.thevpc.nuts.NApplication;
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.NWorkspace;
+import net.thevpc.nuts.io.NPrintStream;
+import net.thevpc.nuts.util.NMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringNutsApplication implements NApplication {
     @Autowired
-    private NSession session;
-    
+    private NWorkspace workspace;
+    @Autowired
+    private NPrintStream out;
+
     public static void main(String[] args) {
-        
         SpringApplication.run(SpringNutsApplication.class, args);
     }
 
     @Override
     public void run() {
-        session.out().println("Hello ##Nuts## World!...");
+        out.println("Hello ##Nuts## World!...");
+        out.println(NMsg.ofC("we are running Nuts %s %s %s %s %s %s",
+                workspace.getRuntimeId().getVersion(),
+                workspace.getPlatform(),
+                workspace.getOs(),
+                workspace.getOsDist(),
+                workspace.getArch(),
+                workspace.getDesktopEnvironment()
+        ));
     }
 }
